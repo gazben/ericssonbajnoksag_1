9101c15307e32411a3729f8e820543943aecee90
Gazder Bence <bencegazder@gmail.com>
2017-10-29 11:13:31 +0100

    Add compiled protocol files

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "protocol/Bugfix.capnp.h"
+#include "protocol/Request.capnp.h"
+#include "protocol/Response.capnp.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
